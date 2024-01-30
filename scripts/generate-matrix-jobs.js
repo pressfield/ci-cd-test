4bb30a96ea4a4ec8c8043b97ef913d0e1118f922
Richard King <richrdkng@gmail.com>
2024-01-30 18:10:48 +0100

    feat: a

--- scripts/generate-matrix-jobs.js
+++ scripts/generate-matrix-jobs.js
@@ -1,8 +1,7 @@
 console.log(JSON.stringify({
   include: [
-    { foo: 'foo - 1', bar: 'bar - 1' },
-    { foo: 'foo - 1', bar: 'bar - 2' },
-    // { foo: 'foo - 2', bar: 'bar - 1' },
+    { id: 'id-1' },
+    { id: 'id-2' },
   ]
 }))
 
