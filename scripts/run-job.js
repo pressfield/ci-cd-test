f3bc1a24151fe3da5008865a1b3ddc52c229e51a
Richard King <richrdkng@gmail.com>
2024-01-31 12:10:01 +0100

    feat: a

--- scripts/run-job.js
+++ scripts/run-job.js
@@ -1,1 +1,5 @@
+console.log(process.argv)
+
+console.log('------------------------')
+
 console.log(process.env)
--- scripts/run-job.sh
+++ scripts/run-job.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-node scripts/run-job.js
+node scripts/run-job.js "$@"
