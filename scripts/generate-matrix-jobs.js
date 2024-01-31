6e53631bcd229da6559b4f1b81e24fb3b927635f
Richard King <richrdkng@gmail.com>
2024-01-31 11:41:26 +0100

    feat: a

--- scripts/generate-matrix-jobs.js
+++ scripts/generate-matrix-jobs.js
@@ -3,18 +3,6 @@ console.log(JSON.stringify({
     { id: 'id-1' },
     { id: 'id-2' },
     { id: 'id-3' },
-    { id: 'id-4' },
-    { id: 'id-5' },
-    { id: 'id-6' },
-    { id: 'id-7' },
-    { id: 'id-8' },
-    { id: 'id-9' },
-    { id: 'id-10' },
-    { id: 'id-11' },
-    { id: 'id-12' },
-    { id: 'id-13' },
-    { id: 'id-14' },
-    { id: 'id-15' },
   ]
 }))
 
